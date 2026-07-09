ab0e823773a94e3924707bfc5ddf5914e68d3a99
NamSE182878 <tranphuongnam1205@gmail.com>
2026-07-09 12:19:50 +0700

    OTP

---
+++ market-place-app/marketplace/src/main/java/market/campus/com/dto/VerifyRegisterOtpRequest.java
@@ -0,0 +1,32 @@
+package market.campus.com.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class VerifyRegisterOtpRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "OTP is required")
+    @Size(min = 6, max = 6, message = "OTP must be 6 digits")
+    private String otp;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+}
